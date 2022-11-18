057b1c2595caeeccbb8ff26dabd32f66c00b36d0
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2022-11-18 08:59:46 -0500

    google-services.json

--- app/src/main/java/com/example/lab5/LoginActivity.java
+++ app/src/main/java/com/example/lab5/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -31,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
 
+        FirebaseApp.initializeApp(this);
         // initialize firebase auth
         mAuth = FirebaseAuth.getInstance();
 
