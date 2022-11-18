a66a6ee603778d51d1df2f6295e87c80851a1e20
oliver <oliver.bustamante@pucp.edu.pe>
2022-11-18 09:54:44 -0500

    avances en insertar

---
+++ app/src/main/java/com/example/lab5/ActivityInsertar.java
@@ -0,0 +1,20 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityInsertar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar);
+    }
+
+    public void subirImagen(View view){
+
+    }
+
+}
