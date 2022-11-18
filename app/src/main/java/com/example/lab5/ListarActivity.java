3388a5c1d53d0001f95accb94f2ee11deaeaf4f5
Joau0824 <a20182162@pucp.edu.pe>
2022-11-18 08:53:23 -0500

    storage

--- app/src/main/java/com/example/lab5/ListarActivity.java
+++ app/src/main/java/com/example/lab5/ListarActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListarActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listar);
     }
 
+    
 
 }
