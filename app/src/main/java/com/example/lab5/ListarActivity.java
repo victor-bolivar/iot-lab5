409ad5c6d9997fb06d239d457fa94cb8ee8a7397
Joau0824 <a20182162@pucp.edu.pe>
2022-11-18 09:51:17 -0500

    Listar

--- app/src/main/java/com/example/lab5/ListarActivity.java
+++ app/src/main/java/com/example/lab5/ListarActivity.java
@@ -3,16 +3,43 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
 public class ListarActivity extends AppCompatActivity {
 
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance();
     }
 
-    
+    StorageReference storageReference = storage.getReference();
+    StorageReference fileRef = storageReference.child("files/uid");
+    //private DatabaseReference mDatabase;
+    public void listarAcrchivos(View view){
+        fileRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                int cantidadElementos = listResult.getItems().size();
+                Log.d("infoApp", String.valueOf(cantidadElementos));
+                for (StorageReference item : listResult.getItems()){
+                    Log.d("InfoAppArchivo",item.getName());
+                }
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.storage.StorageReference;
 
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void listar (View view){
+        Intent intent = new Intent(this,ListarActivity.class);
+        startActivity(intent);
     }
 
 
