b500c60b108f30e4e5f16934b25d3eb618796148
Josue Romero <josueromero1901@gmail.com>
2022-11-20 16:31:46 -0500

    vista

---
+++ app/src/main/java/com/example/lab5/ActualizarActivity.java
@@ -0,0 +1,17 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar);
+    }
+
+
+
+}
